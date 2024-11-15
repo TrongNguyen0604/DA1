2fd0a9acae59ae8f6ab615af6d86097492192307
trongnv <trongnguyen06042005@gmail.com>
2024-11-15 08:04:07 +0700

    hkvk

---
+++ commons/env.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL'       , 'http://localhost/DUANMAU/');
+define('BASE_URL_ADMIN' , 'http://localhost/Project_ShoeNikes/');
+
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'du_an_1');
+
+define('PATH_ROOT'    , __DIR__ . '/../');
--- index.php
+++ index.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Abc</h1>
-</body>
-</html>
+<?php
+
+// Require file Common
+require_once '../commons/env.php'; //khai báo biến từ môi trường 
+
+// Require toàn bộ file Models
+require_once './models/course.php';
+
+// Require toàn bộ file Controllers
+require_once './controllers/controllerCourse.php';
+
+//route
+$act = $_GET['act'] ?? '/';
+
+match ($act){
+    '/'=> (new DashboardController)->dashboard(),
+    
+}
+
+
+
+
+?>
