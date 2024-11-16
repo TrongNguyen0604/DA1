47c678226f241390d57ecae56a07348ea7a12fbd
trongnv <trongnguyen06042005@gmail.com>
2024-11-16 19:40:12 +0700

    env

---
+++ env.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL'       , 'http://localhost/DUANMAU/');
+define('BASE_URL_ADMIN' , 'http://localhost/DUANMAU/admin/');
+define('BASE_URL_CLIENT' , 'http://localhost/DUANMAU/client/');
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'du_an_1');
+
+define('PATH_ROOT'    , __DIR__ . '/../');
