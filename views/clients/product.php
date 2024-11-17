04163c85a6aedde1e6c4ae19412be6a6169fa6a0
trongnv <trongnguyen06042005@gmail.com>
2024-11-17 10:10:17 +0700

    gfnd

---
+++ views/clients/product.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/Styles.css">
+    <script src="https://kit.fontawesome.com/3efea8ee37.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="See_What_New">
+<div class="See_product">
+    
+                <div class="product1">
+                    <div class="product_img">
+                        <img src="Image/See1.png" alt="">
+                    </div>
+                    <div class="product_Content">
+                        <div class="name">
+                            Air Jordan 1 Low SE
+                        </div>
+                        <p>
+                            Women's Shoes
+                        </p>
+                        <div class="price">
+                            3,959,000₫
+                        </div>
+                    </div>
+                </div>
+
+                
+
+            </div>
+            </div>
+</body>
+</html>
